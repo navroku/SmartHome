c225bb9b433c80eddb93b8043b1e5734b8157ffa
Karlis <karlis.laskovs@gmail.com>
2023-11-14 23:37:42 +0200

    UI; working logout

--- SmartHomeServer/public/scripts/script.js
+++ SmartHomeServer/public/scripts/script.js
@@ -1,24 +1,6 @@
-let menuIcon = document.querySelector('.menuIcon');
-let nav = document.querySelector('.overlay-menu');
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-menuIcon.addEventListener('click', () => {
-    if (nav.style.transform != 'translateX(0%)') {
-        nav.style.transform = 'translateX(0%)';
-        nav.style.transition = 'transform 0.2s ease-out';
-    } else { 
-        nav.style.transform = 'translateX(-100%)';
-        nav.style.transition = 'transform 0.2s ease-out';
-    }
-});
-
-
-// Toggle Menu Icon ========================================
-let toggleIcon = document.querySelector('.menuIcon');
-
-toggleIcon.addEventListener('click', () => {
-    if (toggleIcon.className != 'menuIcon toggle') {
-        toggleIcon.className += ' toggle';
-    } else {
-        toggleIcon.className = 'menuIcon';
-    }
-});
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
--- SmartHomeServer/routes/logout.js
+++ SmartHomeServer/routes/logout.js
@@ -3,12 +3,14 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/logout', (req, res) => {
-  // Destroy the session to log out the user
-  req.session.destroy((err) => {
+  // Destroy the session to log the user out
+  req.session.destroy(err => {
     if (err) {
       console.error('Error destroying session:', err);
+      res.sendStatus(500);
+    } else {
+      res.redirect('/'); // Redirect to the home page after logout
     }
-    res.redirect('/login'); // Redirect to the login page after logout
   });
 });
 
